b12314e4868313f0b72b4eb89ebad17544f43a90
andi2631 <acorrotti@gmail.com>
2022-02-02 13:22:10 -0300

    ergerg

--- react-redux-ttypescript/src/NewNoteInput.tsx
+++ react-redux-ttypescript/src/NewNoteInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
 //esta interfaz define las propiedades de mi componente
 interface NewNoteInputProps{
@@ -6,10 +6,18 @@ interface NewNoteInputProps{
   }
 
   //este componente recibe las propiedades de la interfaz definida arriba
-export const NewNoteInput:React.FC<NewNoteInputProps> = () => {
+export const NewNoteInput:React.FC<NewNoteInputProps> = ({addNote}) => {//hago destructuring de los props
+
+  const [note, setNote] = React.useState("")
+
+  const updateNote = (event: ChangeEvent<HTMLInputElement>) => { //el tipo de dato de event es el evento de ChangeEvent
+    setNote(event.target.value) //se setea la nota con el valor
+
+  }
+  
   return (
     <div>
-      <input type="text" name="note" placeholder='note'></input>
+      <input onChange={updateNote} type="text" name="note" value={note} placeholder='note'></input>
       <button>Add note</button>
     </div>
   );
